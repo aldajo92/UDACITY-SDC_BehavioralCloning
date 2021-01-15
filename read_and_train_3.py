33bdc3ae73950f1ee038babc87b8404056bd1605
aldajo92 <alejodiscovery20@gmail.com>
2021-01-15 00:40:28 -0500

    created read and train 3

---
+++ read_and_train_3.py
@@ -0,0 +1,47 @@
+import csv
+import cv2
+import numpy as np
+
+dataPath = 'data/'
+
+lines = []
+print('Reading from: ./{}'.format(dataPath))
+with open('./{}driving_log.csv'.format(dataPath)) as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './{}IMG/{}'.format(dataPath,filename)
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+Y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Conv2D(filters=6, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D())
+model.add(Conv2D(filters=6, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss = 'mse', optimizer = 'adam')
+model.fit(X_train, Y_train, validation_split = 0.2, shuffle = True, nb_epoch=2)
+
+model.save('{}model.h5'.format(dataPath))
