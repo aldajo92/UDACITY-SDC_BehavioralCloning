5fe7418652830c5e8bc23782f88ff4736880e7a9
aldajo92 <alejodiscovery20@gmail.com>
2021-01-15 22:41:33 -0500

    added cropping image process on read and train 5

---
+++ read_and_train_5.py
@@ -0,0 +1,55 @@
+import csv
+import cv2
+import numpy as np
+
+dataPath = 'data/'
+
+lines = []
+print('Reading from: ./{}'.format(dataPath))
+with open('./{}driving_log.csv'.format(dataPath)) as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './{}IMG/{}'.format(dataPath,filename)
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+Y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPool2D, Cropping2D
+from keras.layers.convolutional import Convolution2D
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Conv2D(filters=6, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D())
+model.add(Conv2D(filters=6, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss = 'mse', optimizer = 'adam')
+model.fit(X_train, Y_train, validation_split = 0.2, shuffle = True, nb_epoch=2)
+
+model.save('model.h5'.format(dataPath))
